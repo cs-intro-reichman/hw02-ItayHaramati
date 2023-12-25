0beb18fcccf6d024d8bdae333fb7fff1d94f4bfa
Itay <you@example.com>
2023-12-25 18:46:22 +0200

    I changed Reverse

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,13 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String str = args[0];
+		String reverseString = "";
+		int n = str.length();
+		for(int i = n - 1; i >= 0; i--){
+			reverseString = reverseString + str.charAt(i);
+		}
+		System.out.println(reverseString); 
+		System.out.println("The middle character is " + str.charAt((str.length() - 1) / 2));
 	}
 }
