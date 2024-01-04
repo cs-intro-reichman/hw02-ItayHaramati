7ca8857f018d0534d1d9a65e1104c0ceca4d2b64
Itay <you@example.com>
2024-01-04 07:55:55 +0200

    I changed DamkaBoard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,15 +4,18 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]); 
-		for(int i = 0; i < n; i++){
-			for (int m = n - i; m % 2 == 0; m++){
-				System.out.print("  ");}   
+		for(int i = 0; i < n; i++){  
 			for(int j = 0; j < n; j++){
-				System.out.print("* ");}
-		System.out.println(" ");
+				if(i % 2 == 0){
+					System.out.print("* ");}
+				else{
+					System.out.print(" *");
+				}
+				}
+			System.out.println(" ");}
 			}
 		}
-	}
+	
 
 
  
