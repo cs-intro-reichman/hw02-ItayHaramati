76ab2ec47407294517c65e42edf060e3abec4e51
Itay <you@example.com>
2024-01-04 09:48:08 +0200

    I changed Perfect

--- Perfect.java
+++ Perfect.java
@@ -4,14 +4,16 @@
 public class Perfect {
 	public static void main (String[] args) {
 		int N = Integer.parseInt(args[0]);
+		String s = N + " is a perfect number since " + N + " = " + 1;
 		int sum = 1;
 		for(int i = 2; i < N; i++){
 			if(N % i == 0){
+				s = s + " + " + i;
 				sum += i;
 			}
 		}
 		if (sum == N){
-			System.out.println(N + " is a perfect number");
+			System.out.println(s);
 		}else{
             System.out.println(N + " is not a perfect number");
     }
